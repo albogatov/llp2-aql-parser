82c68092b8f5edeb6a40eecfbb2c8eb8d3403e81
abogatov <abogatov@solanteq.com>
2023-01-22 01:14:05 +0300

    AST writer WIP

---
+++ ast.c
@@ -0,0 +1,183 @@
+#include <stdlib.h>
+#include "string_utils.h"
+
+enum Comparison reverse_Comparison(enum Comparison val) {
+    if (val < 3 || val > 5) return 8 - val;
+    return val;
+}
+
+Node* new_name(const char* v_first, const char* v_second) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = NAME_my;
+    new_node->v_first.str = str_copy(v_first);
+    if (v_second != NULL)
+        new_node->v_second.str = str_copy(v_second);
+    new_node->first = NULL;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+
+Node* new_string(const char* v_first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = STRING_my;
+    new_node->v_first.str = str_copy(v_first);
+    new_node->first = NULL;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+
+Node* new_integer(int v_first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = INTEGER_my;
+    new_node->v_first.integer = v_first;
+    new_node->first = NULL;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+
+Node* new_float(float v_first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = FLOAT_my;
+    new_node->v_first.flt = v_first;
+    new_node->first = NULL;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+
+Node* new_bool(bool v_first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = BOOL_my;
+    new_node->v_first.boolean = v_first;
+    new_node->first = NULL;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+
+Node* new_type(enum DataType v_first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = TYPE_my;
+    new_node->v_first.data_type = v_first;
+    new_node->first = NULL;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+
+Node* new_list(Node* first, Node* second) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = LIST_my;
+    new_node->first = first;
+    new_node->second = second;
+    new_node->third = NULL;
+    return new_node;
+}
+
+Node* new_pair(const char* v_first, Node* second) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = PAIR_my;
+    new_node->v_first.str = str_copy(v_first);
+    new_node->first = NULL;
+    new_node->second = second;
+    new_node->third = NULL;
+    return new_node;
+}
+Node* new_select(const char* v_first, Node* first, const char* v_second, Node* second, Node* third) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = FOR_my;
+    new_node->v_first.str = str_copy(v_first);
+    if (v_second != NULL) new_node->v_second.str = str_copy(v_second);
+    else new_node->v_second.str = NULL;
+    new_node->first = first;
+    new_node->second = second;
+    new_node->third = third;
+    return new_node;
+}
+Node* new_delete(const char* v_first, Node* first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = REMOVE_my;
+    new_node->v_first.str = str_copy(v_first);
+    new_node->first = first;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+Node* new_insert(const char* v_first, Node* first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = INSERT_my;
+    new_node->v_first.str = str_copy(v_first);
+    new_node->first = first;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+Node* new_update(const char* v_first, Node* first, Node* second) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = UPDATE_my;
+    new_node->v_first.str = str_copy(v_first);
+    new_node->first = first;
+    new_node->second = second;
+    new_node->third = NULL;
+    return new_node;
+}
+Node* new_create(const char* v_first, Node* first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = CREATE_my;
+    new_node->v_first.str = str_copy(v_first);
+    new_node->first = first;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+Node* new_drop(const char* v_first) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = DROP_my;
+    new_node->v_first.str = str_copy(v_first);
+    new_node->first = NULL;
+    new_node->second = NULL;
+    new_node->third = NULL;
+    return new_node;
+}
+Node* new_where(enum LogicOperation v_first, Node* first, Node* second) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = FILTER_my;
+    new_node->v_first.log_op = v_first;
+    new_node->first = first;
+    new_node->second = second;
+    new_node->third = NULL;
+    return new_node;
+}
+Node* new_compare(enum Comparison v_first, Node* first, Node* second) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->type = COMPARE_my;
+    new_node->v_first.comp = v_first;
+    new_node->first = first;
+    new_node->second = second;
+    new_node->third = NULL;
+    return new_node;
+}
+
+void close_tree(Node* root) {
+    if (root == NULL) return;
+    close_tree(root->first);
+    close_tree(root->second);
+    close_tree(root->third);
+    if (root->type == NAME_my ||
+        root->type == STRING_my ||
+        root->type == FOR_my ||
+        root->type == REMOVE_my ||
+        root->type == INSERT_my ||
+        root->type == UPDATE_my ||
+        root->type == CREATE_my ||
+        root->type == DROP_my ||
+        root->type == PAIR_my)
+        free(root->v_first.str);
+    if (root->type == NAME_my ||
+        root->type == FOR_my)
+        free(root->v_second.str);
+    free(root);
+}
---
+++ ast.h
@@ -0,0 +1,106 @@
+#pragma once
+#include <stdbool.h>
+
+enum NodeType {
+    NAME_my,
+    STRING_my,
+    INTEGER_my,
+    FLOAT_my,
+    BOOL_my,
+    LIST_my,
+    PAIR_my,
+    FOR_my,
+    INSERT_my,
+    REMOVE_my,
+    UPDATE_my,
+    CREATE_my,
+    DROP_my,
+    FILTER_my,
+    COMPARE_my,
+    TYPE_my
+};
+
+enum DataType {
+    STR_my,
+    INT_my,
+    FLT_my,
+    BOOLEAN_my
+};
+
+static const char* DataType_strings[] = {
+        "STR",
+        "INT",
+        "FLT",
+        "BOOLEAN"
+};
+
+enum LogicOperation {
+    AND_my,
+    OR_my
+};
+
+static const char* LogicOperation_strings[] = {
+        "&&",
+        "||"
+};
+
+enum Comparison {
+    GREATER = 1, GREATER_OR_EQUAL = 2, LESS = 3, LESS_OR_EQUAL = 4, EQUAL = 5, NOT_EQUAL = 6, NO_COMPARE = 7,
+};
+
+enum Comparison reverse_Comparison(enum Comparison val);
+
+static const char* Comparison_strings[] = {
+        "CONTAINS",
+        ">",
+        ">=",
+        "==",
+        "NO_COMPARE",
+        "!=",
+        "=<",
+        "<",
+        "IS SUBSTR OF"
+};
+
+union Value {
+    char* str;
+    enum LogicOperation log_op;
+    enum Comparison comp;
+    int integer;
+    float flt;
+    bool boolean;
+    enum DataType data_type;
+};
+
+typedef struct Node Node;
+typedef union Value Value;
+typedef enum NodeType NodeType;
+
+struct Node {
+    NodeType type;
+    Value v_first;
+    Value v_second;
+    Node* first;
+    Node* second;
+    Node* third;
+};
+
+Node* new_name(const char* v_first, const char* v_second);
+Node* new_string(const char* v_first);
+Node* new_integer(int v_first);
+Node* new_float(float v_first);
+Node* new_bool(bool v_first);
+Node* new_type(enum DataType v_first);
+Node* new_list(Node* first, Node* second);
+Node* new_pair(const char* v_first, Node* second);
+Node* new_select(const char* v_first, Node* first, const char* v_second, Node* second, Node* third);
+Node* new_delete(const char* v_first, Node* first);
+Node* new_insert(const char* v_first, Node* first);
+Node* new_update(const char* v_first, Node* first, Node* second);
+Node* new_create(const char* v_first, Node* first);
+Node* new_drop(const char* v_first);
+Node* new_where(enum LogicOperation v_first, Node* first, Node* second);
+Node* new_compare(enum Comparison v_first, Node* first, Node* second);
+
+void close_tree(Node* root);
+
